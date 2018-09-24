fbb8ca290f5e0856998c79951ff64d85c8c16409
y-collin <>
2018-09-24 21:20:48 +0900

    list, update

--- src/main/java/com/example/webpractice/UserListController.java
+++ src/main/java/com/example/webpractice/UserListController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,14 +14,29 @@ public class UserListController {
 	@Autowired
 	private UserRepository userRepository;
 
-	   @GetMapping("/users")
+	   @GetMapping("/users/list")
 	    public String list(@RequestParam(name="name", required=false, defaultValue="user1") String name, Model model) {
 		    model.addAttribute("name", name);
-		    model.addAttribute("count" , userRepository.count());
-
-
 		    model.addAttribute("userList", userRepository.findAll());
+	        return "users/list";
+	    }
 
-	        return "users";
+
+	   @GetMapping("/users/update")
+	    public String update(@RequestParam(name="id", required=true, defaultValue="1") Integer id, Model model) {
+		    model.addAttribute("id", id);
+		    model.addAttribute("user", userRepository.findById(id).get());
+
+	        return "users/update";
+	    }
+
+
+
+	   @PostMapping("/users/update")
+	    public String update(@ModelAttribute  User user, Model model) {
+		   userRepository.save(user);
+		   model.addAttribute("user", userRepository.findById(user.getId()).get());
+
+	        return "redirect:list";
 	    }
 }
